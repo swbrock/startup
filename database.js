055309787363bcffce5232f94b9c2fa958df1e6f
swbrock <90523707+swbrock@users.noreply.github.com>
2023-04-18 22:40:39 -0600

    update deploy file and update database

--- database.js
+++ database.js
@@ -1,97 +1,133 @@
 const { MongoClient } = require('mongodb');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
-
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
-const hostname = process.env.MONGOHOSTNAME;
+const hostname = "thewaal.yj9rypo.mongodb.net";
 
 if (!userName) {
-    throw error ('Database not configured');
+  throw Error('Database not configured. Set environment variables');
 }
 
-async function test() {
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 
-    const url = `mongodb+srv://${userName}:${password}@${hostname}`;
-
-    const client = new MongoClient(url);
-    const userCollection = client.db('TheWaal').collection('users');
-    const eventCollection = client.db('TheWaal').collection('events');
-    const houseCollection = client.db('TheWaal').collection('houses');
-
-    const house = {
-        name: 'The Waal',
-        address: '1234 Main St',
-        city: 'Salt Lake City',
-        state: 'UT',
-        zip: '84101',
-    };
-    await houseCollection.insertOne(house);
-
-    const query = {state: 'UT'};
-    const options = {
-        sort: { name: 1 },
-        limit: 10,
-    };
+const client = new MongoClient(url);
+const userCollection = client.db('TheWaal').collection('users');
+const scoreCollection = client.db('TheWaal').collection('scores');
+const movieCollection = client.db('TheWaal').collection('movies');
+const ratingCollection = client.db('TheWaal').collection('ratings');
 
 
-    const cursor = collection.find(query, options);
-    const result = await cursor.toArray();
-    console.log(result);
-
-    }
-test().catch(console.error);
-
-
-
-
-async function test() {
-    await client.connect();
-    const result = await userCollection.insertOne(me);
-    console.log(result);
+//rating includes movie id, user id, rating
+async function addRating(rating) {
+  await ratingCollection.insertOne(rating);
 }
 
-test();
+//get all ratings by movie id
+async function getRatingsByMovieId(id) {
+  const query = { movieId: id };
+  const options = {
+    sort: { rating: 1 },
+  };
+  const cursor = ratingCollection.find(query, options);
+  return cursor.toArray();
+}
+
+//get all ratings by user id
+async function getRatingsByUserId(id) {
+  const query = { userId: id };
+  const options = {
+    sort: { rating: 1 },
+  };
+  const cursor = ratingCollection.find(query, options);
+  return cursor.toArray();
+}
+main().catch(console.error);
+
+// add movie to database
+async function addMovie(movie) {
+  await movieCollection.insertOne(movie);
+}
+
+// get all movies from database
+async function getMovies() {
+  const query = {};
+  const options = {
+    sort: { title: 1 },
+  };
+  const cursor = movieCollection.find(query, options);
+  return cursor.toArray();
+}
+
+// get movie by id
+async function getMovieById(id) {
+  const query = { _id: id };
+  const options = {
+    sort: { title: 1 },
+  };
+  const cursor = movieCollection.find(query, options);
+  return cursor.toArray();
+}
+
+// get movie by title
+async function getMovieByTitle(title) {
+  const query = { title: title };
+  const options = {
+    sort: { title: 1 },
+  };
+  const cursor = movieCollection.find(query, options);
+  return cursor.toArray();
+}
+
 
 function getUser(email) {
-    return userCollection.findOne({ email: email });
+  return userCollection.findOne({ email: email });
 }
 
 function getUserByToken(token) {
-    return userCollection.findOne({ token: token });
+  return userCollection.findOne({ token: token });
 }
 
 async function createUser(email, password) {
-    const passwordHash = await bcrypt.hash(password, 10);
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
 
-    const user = {
-        email: email,
-        password: passwordHash,
-        token: uuid.v4()
-    };
-    await userCollection.insertOne(user);
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
 
-    return user;
+  return user;
 }
 
-function addEvent(event) {
-    return eventCollection.insertOne(event);
+function addScore(score) {
+  scoreCollection.insertOne(score);
 }
 
-function getEvents() {
-    const query = {};
-    const options = {
-        sort: { startDate: 1 },
-        limit: 10,
-    };
-    const cursor = eventCollection.find(query, options);
-    return cursor.toArray();
+function getHighScores() {
+  const query = {};
+  const options = {
+    sort: { score: -1 },
+    limit: 10,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
 }
+
 
 module.exports = {
-    getUser,
-    getUserByToken,
-    createUser,
-    addEvent,
-    getEvents,
+  getUser,
+  getUserByToken,
+  createUser,
+  addScore,
+  getHighScores,
+  addMovie,
+  getMovies,
+  getMovieById,
+  getMovieByTitle,
+  addRating,
+  getRatingsByMovieId,
+  getRatingsByUserId,
 };
