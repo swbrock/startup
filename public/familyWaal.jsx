b914f31eb7b8dd591edf7dcabdcc74fd631405bf
swbrock <90523707+swbrock@users.noreply.github.com>
2023-04-19 00:04:50 -0600

    Add react pages

---
+++ public/familyWaal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class FamilyWaal extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Family Waal</h1>
+            </div>
+        );
+    }
+}
